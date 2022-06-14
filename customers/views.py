929cc29d75c4f5247353ef89d1e765086f0c300c
clownkill <alehnovich90@yandex.ru>
2022-06-14 16:27:52 +0300

    add customers html

--- customers/views.py
+++ customers/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class Index(View):
+
+    def get(self, request, *args, **kwargs):
+        return render(request, 'customers/index.html')
+
+
+class About(View):
+
+    def get(self, request, *args, **kwargs):
+        return render(request, 'customers/about.html')
--- fooddelivery/urls.py
+++ fooddelivery/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from customers.views import Index, About
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', Index.as_view(), name='index'),
+    path('about/', About.as_view(), name='about'),
 ]
